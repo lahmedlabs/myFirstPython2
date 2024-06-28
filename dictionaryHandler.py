c5400c182952ab384e33d4fbd3a3bd25c2f2653b
lmlabs <lahmedlabs@gmail.com>
2024-06-28 17:59:00 +0900

    init

---
+++ dictionaryHandler.py
@@ -0,0 +1,13 @@
+user  = {}
+print(user)
+print(type(user))
+
+#sprint2
+user  = {"name":"James"}
+print(user)
+
+#3
+class1 = {0:"James", 1:"Victoria"}
+print(class1)
+print(class1[0])
+print(class1[1])
